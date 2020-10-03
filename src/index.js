86035508babbcfcd636028f3f5d28c4c989bec4b
Mornival <55550567+Mornival@users.noreply.github.com>
2020-10-03 16:23:36 +0300

    fix: fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array)){
+    if(array.length == 0){
+      return 0;
+    }
+     if(array.length > 0){
+       minNumber = array[0];
+      for(i = 0; i < array.length; i++)
+      {
+        if(array[i] < minNumber)
+        {
+          minNumber = array[i];
+        }
+      }
+      return minNumber;
+     }
+  }
+  else{
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array)){
+    if(array.length == 0){
+      return 0;
+    }
+    if(array.length > 0){
+      maxNumber = array[0];
+      for(i = 0; i < array.length; i++){
+        if(array[i] > maxNumber)
+        {
+          maxNumber = array[i];
+        }
+      }
+      return maxNumber;
+    }
+  }
+  else{
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array)){
+    if(array.length == 0){
+      return 0;
+    }
+    if(array.length > 0)
+    {
+      let avgNumber = 0;
+      for(i = 0;i < array.length ; i++){
+  
+        avgNumber += array[i];
+      }
+      avgNumber /= array.length;
+      return avgNumber;
+    }
+  }
+  else{
+    return 0;
+  }
 }
